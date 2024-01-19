0a7d6dd17cbb2c54019e636f6c1572011df6670f
jesussalas813 <masalas787@misena.edu.co>
2024-01-18 19:38:12 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Miboleta',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
